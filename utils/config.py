42fd922432706fb99417a5faed25c2151686c019
Yu <shaoyenyu@gmail.com>
2017-09-19 01:10:15 +0800

    #

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine
 
-default_engine = create_engine("mysql+pymysql://root:15901622959qxmt@127.0.0.1:3306/security", connect_args={"charset": "utf8"})
+default_engine = create_engine("mysql+pymysql://root:15901622959qxmt@127.0.0.1:3306/babylon", connect_args={"charset": "utf8"})
