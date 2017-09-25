fc1d20a278d0670dc7a4c1cc04631455c141d358
Yu <shaoyenyu@gmail.com>
2017-09-26 00:51:02 +0800

    # 添加日频k线至定时采集任务;

--- Scripts/SCHEDULER/cron.py
+++ Scripts/SCHEDULER/cron.py
@@ -7,7 +7,7 @@ from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 
 from Scripts.ETL import stock_info
 from Scripts.CRAWL import (
-    stock_info_hist, stock_kdata_5, stock_kdata_15, stock_kdata_30, stock_kdata_60
+    stock_info_hist, stock_kdata_5, stock_kdata_15, stock_kdata_30, stock_kdata_60, stock_kdata_d
 )
 
 jobstores = {
@@ -36,6 +36,7 @@ def main():
     scheduler.add_job(stock_kdata_15.main, "cron", day_of_week="mon-fri", hour="10-12,14-16,18,21,23", minute="40")
     scheduler.add_job(stock_kdata_30.main, "cron", day_of_week="mon-fri", hour="10-12,14-16,18,21,23", minute="45")
     scheduler.add_job(stock_kdata_60.main, "cron", day_of_week="mon-fri", hour="10-12,14-16,18,21,23", minute="5")
+    scheduler.add_job(stock_kdata_d.main, "cron", day_of_week="mon-fri", hour="5,11,17,23", minute="5")
     # scheduler.add_job(stock_kdata_5.main, "cron", minute=5)
     # scheduler.add_job(stock_kdata_15.main, "cron", minute=10)
     # scheduler.add_job(stock_kdata_30.main, "cron", minute=15)
