f5f0f148b5e283f350702626ab8ad4e45663d4c5
Yu <shaoyenyu@gmail.com>
2018-03-20 09:28:38 +0800

    1.Strategy.test_strategy: 复制通联demo均线策略;

--- Strategy/test_strategy.py
+++ Strategy/test_strategy.py
@@ -21,12 +21,25 @@ class DataYesStrategy(Strategy):
 
     # 第二部分：初始化策略，回测期间只运行一次，可以用于设置全局变量
     # account是回测期间的虚拟交易账户，存储上述全局变量参数信息，并在整个策略执行期间更新并维护可用现金、证券的头寸、每日交易指令明细、历史行情数据等。
-    def initialize(account):
+    def initialize(self, account):
         # account.i = 1
         pass
 
     # 第三部分：策略每天下单逻辑，执行完成后，会输出每天的下单指令列表
     # 这个函数在每个交易日开盘前被调用，模拟每个交易日开盘前，交易策略会根据历史数据或者其他信息进行交易判断，生成交易指令
-    def handle_data(account):
-        # 这里编写下单逻辑
-        return
+    def handle_data(self, account):
+        # account.get_attribute_history：表示获取所有证券过去20天的closePrice数据，返回数据类型为 dict，键为每个证券的secID
+        hist = account.get_attribute_history('closePrice', 20)
+        for stk in account.universe:
+            # 计算股票过去5天收盘平均值
+            ma5 = hist[stk][-5:].mean()
+            # 计算股票过去20天收盘平均值
+            ma20 = hist[stk][:].mean()
+
+            # 如果5日均线大于20日均线，并且该股票当前没有持仓，则买入100手
+            # account.valid_secpos：表示当前交易日持有数量大于0的证券头寸。数据类型为字典，键为证券代码，值为头寸。
+            if ma5 > ma20 and stk not in account.valid_secpos:
+                order(stk, 10000)
+            # 如果5日均线小于20日均线，则该股票全部卖出
+            elif ma5 <= ma20:
+                order_to(stk, 0)
