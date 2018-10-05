ac487a98a7214e0278bfbec13791901fd1c413b1
Yu <shaoyenyu@gmail.com>
2018-10-05 20:24:05 +0800

    1. utils.algorithm.dummy: 1) pandas: add function rolling_apply2d to replace `pandas.DataFrame.rolling().apply()`

--- utils/algorithm/dummy/numpy.py
+++ utils/algorithm/dummy/numpy.py
@@ -9,26 +9,6 @@ def rolling2d(arr: np.array, window: int):
         Given a ndarray of shape(2500, 3600), window size = 3,
         then this will generate a new view of shape (2498, 3, 3600)
 
-    e.g.2
-        import numpy as np
-        import pandas as pd
-
-        shape = (1000, 2000)
-        lst = [np.nan, *list(np.random.rand(*shape).flatten())[:-1]]
-
-        arr = np.array(lst).reshape(shape)
-        df = pd.DataFrame(arr)
-
-        window = 3
-        f1 = lambda x: x.sum(1) + 1
-        f2 = lambda x: x.sum(0) + 1
-
-        f1(dummy.rolling(arr, window))  # 15.2 ms ± 87.4 µs
-        df.rolling(window).apply(f2)  # 4.16 s ± 198 ms
-
-        np.mean(dummy.rolling(arr, window), 1)  # 10.1 ms ± 96.3 µs
-        df.rolling(window).apply(np.mean)  # 8.44 s ± 87.3 ms
-
     Args:
         arr: numpy.ndarray
             two-dimension-ndarray
--- utils/algorithm/dummy/pandas.py
+++ utils/algorithm/dummy/pandas.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from utils.algorithm.dummy import numpy as npd
 
 
 def combine_first(left: pd.DataFrame, right: pd.DataFrame, is_type_safe=False):
@@ -10,3 +11,41 @@ def combine_first(left: pd.DataFrame, right: pd.DataFrame, is_type_safe=False):
         return pd.DataFrame(np.where(left.notnull().values, left.values, right.values), left.index, left.columns)
     else:
         return pd.DataFrame(np.where(np.isnan(left.values), right.values, left.values), left.index, left.columns)
+
+
+def rolling_apply2d(obj: pd.DataFrame, window: int, func: callable):
+    """
+
+    Args:
+        obj: pandas.DataFrame
+        window: int
+        func: callable
+            func1d
+
+    e.g.1
+        import numpy as np
+        import pandas as pd
+
+        shape = (50, 100)
+        lst = [np.nan, *list(np.random.rand(*shape).flatten())[:-1]]
+
+        arr = np.array(lst).reshape(shape)
+        df = pd.DataFrame(arr)
+
+        window = 3
+
+        f = lambda x: x.sum(0) + 1
+        rolling_apply(df, window, f)  # 15.2 ms ± 87.4 µs
+        df.rolling(window).apply(f)  # 4.16 s ± 198 ms
+
+        f = lambda x: x.mean(0)
+        rolling_apply(df, 3, f)  # 13.1 ms ± 46.7 µs
+        df.rolling(window).apply(f)  # 6.92 s ± 70.3
+
+    Returns:
+        pandas.DataFrame
+
+    """
+
+    val = func(np.swapaxes(npd.rolling2d(obj.values, window).T, 0, 1)).T
+    return pd.DataFrame(val, obj.index[-len(val):], obj.columns)
